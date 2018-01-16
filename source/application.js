7c505652fe5baff793d18732287ef7929dba7fd7
Grigore Stefan <g_stefan@yahoo.com>
2018-01-16 02:48:37 +0200

    update

--- source/application.js
+++ source/application.js
@@ -13,7 +13,7 @@ BrowserAutomaton.allowedLink=[];
 
 BrowserAutomaton.getOptions=function(fnNext) {
 	chrome.storage.sync.get({
-		allowedLink: '[]'
+		allowedLink: "[]"
 	}, function(items) {
 		try {
 			BrowserAutomaton.allowedLink=JSON.parse(items.allowedLink);
@@ -31,35 +31,71 @@ BrowserAutomaton.fnCheck="44d5333afbc046f0a4a00e86c2b5bbbd35e2fab8d2902d973e8030
 BrowserAutomaton.process=function(elId,tabId,fnName,fnCheck) {
 
 	var automatonElement=document.getElementById(elId);
-	if(!automatonElement) {
-		automatonElement=document.createElement("div");
-		automatonElement.id = elId;
-		automatonElement.style.display = "none";
-		automatonElement.innerHTML = "";
-		document.body.appendChild(automatonElement);
+	if(automatonElement) {
+		return "";
 	};
 
+	automatonElement=document.createElement("div");
+	automatonElement.id = elId;
+	automatonElement.style.display = "none";
 	automatonElement.innerHTML = "";
+	document.body.appendChild(automatonElement);
 
 	var automatonScript = document.createElement("script");
-	automatonScript.textContent = "document.getElementById(\""+elId+"\").innerHTML="+fnName+"();";
+	automatonScript.textContent = "(function(){\r\n"+
+				      "var counter=0;\r\n"+
+				      "var processEvent=function(){\r\n"+
+				      "\tvar retV=\"undefined\";\r\n"+
+				      "\tif(typeof("+fnName+")===\"undefined\"){\r\n"+
+				      "\t\tretV=\"loading\";\r\n"+
+				      "\t}else{\r\n"+
+				      "\t\tretV="+fnName+"();\r\n"+
+				      "\t\t"+fnName+"=function(){return \"undefined\";};\r\n"+
+				      "\t};\r\n"+
+				      "\tif(retV===\"loading\"){\r\n"+
+				      "\t\t++counter;\r\n"+
+				      "\t\tif(counter >= 15){\r\n"+
+				      "\t\t\tdocument.getElementById(\""+elId+"\").innerHTML=\"undefined\";\r\n"+
+				      "\t\t\treturn;\r\n"+
+				      "\t\t};\r\n"+
+				      "\t\tsetTimeout(function(){\r\n"+
+				      "\t\t\tprocessEvent();\r\n"+
+				      "\t\t},1000);\r\n"+
+				      "\t\treturn;\r\n"+
+				      "\t};\r\n"+
+				      "\tdocument.getElementById(\""+elId+"\").innerHTML=retV;\r\n"+
+				      "};\r\n"+
+				      "processEvent();\r\n"+
+				      "})();\r\n";
+
 	(document.head||document.documentElement).appendChild(automatonScript);
 
 	return fnCheck;
 };
 
 BrowserAutomaton.procesResponse=function(tabId) {
-	chrome.tabs.executeScript(tabId, {
-		code: "document.getElementById(\""+BrowserAutomaton.elId+"\").innerHTML;"
-	},function(result) {
-		if((""+result).length>0) {
-			(new Function(atob(""+result)))(tabId);
-			return;
-		};
-		setTimeout(function() {
-			BrowserAutomaton.procesResponse(tabId);
-		},100);
-	});
+	var counter=0;
+	var processEvent=function(){
+		chrome.tabs.executeScript(tabId, {
+			code: "document.getElementById(\""+BrowserAutomaton.elId+"\").innerHTML;"
+		},function(result) {
+			if((""+result).length>0) {
+				if((""+result)==="undefined"){
+					return;
+				};
+				(new Function(atob(""+result)))(tabId);
+				return;
+			};
+			++counter;
+			if(counter>15){
+				return;
+			};
+			setTimeout(function() {
+				processEvent();
+			},1000);
+		});
+	};
+	processEvent();
 };
 
 BrowserAutomaton.procesLink=function(tabId) {
@@ -98,15 +134,34 @@ BrowserAutomaton.procesTab=function(tabId, tab) {
 	};
 };
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-	if (changeInfo.status=="complete") {
+BrowserAutomaton.listenTab=function(tabId,tab) {
+	if(typeof(tab.status) === "undefined") {
+		setTimeout(function() {
+			BrowserAutomaton.listenTab(tabId,tab);
+		},1000);
+		return;
+	};
+
+	if(tab.status === "complete") {
 		BrowserAutomaton.procesTab(tabId, tab);
+	};
+};
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+	if (changeInfo.status === "complete") {
+		BrowserAutomaton.procesTab(tabId, tab);
+		return;
+	};
+	if(typeof(changeInfo.status) === "undefined") {
+		if(tab.status === "complete") {
+			BrowserAutomaton.procesTab(tabId, tab);
+			return;
+		};
+		BrowserAutomaton.listenTab(tabId, tab);
 	};
 });
 
-//
-
-chrome.tabs.getAllInWindow(null, function(tabs) {
+chrome.tabs.query({currentWindow:true,status:"complete"},function(tabs) {
 	for(var k=0; k<tabs.length; ++k) {
 		BrowserAutomaton.procesTab(tabs[k].id, tabs[k]);
 	};
