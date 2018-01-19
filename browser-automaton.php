66c01c6bf495861a504ceaeb3753d45f3890a094
Grigore Stefan <g_stefan@yahoo.com>
2018-01-19 03:00:55 +0200

    update

--- browser-automaton.php
+++ browser-automaton.php
@@ -3,7 +3,7 @@
 //
 // Browser Automaton Extension
 //
-// Copyright (c) 2017 Grigore Stefan, <g_stefan@yahoo.com>
+// Copyright (c) 2018 Grigore Stefan, <g_stefan@yahoo.com>
 // Created by Grigore Stefan <g_stefan@yahoo.com>
 //
 // The MIT License (MIT) <http://opensource.org/licenses/MIT>
@@ -14,13 +14,87 @@
 	<title>Browser Automaton</title>
 	<script>
 		function _3dc656c5131d62c8fac57caec67613bb6ccbb05476c8ad39c785cbf5a5af348d(){
-			return btoa("return ("+fnHelloWorld+").apply(this,arguments);");
+			return btoa("var retV=("+fnAutomaton+")();var init=retV.init;var processUrl=retV.processUrl;");
 		};
 
-		function fnHelloWorld(tabId){
-			chrome.tabs.executeScript(tabId, {
-				code: "alert(\"Hello World!\");"
-			});
+		function fnAutomaton(){
+
+			var protect=function(){
+				var el=document.createElement("div");
+				el.id="browser-automaton-protect";
+				el.style.position = "fixed";
+				el.style.top = "0px";
+				el.style.left = "0px";
+				el.style.width = "100%";
+				el.style.height = "100%";
+				el.style.backgroundColor = "rgba(255,255,255,0.5)";
+				el.style.color = "#000000";
+				el.style.zIndex = "100000";
+				el.style.marginBottom ="0px";
+				el.innerHTML = "<center><div style='font-size:24px;font-weright:bold;top:240px;width:320px;background-color:white;line-height:48px;position:relative;border-radius:8px;'>Protected - ##</div></center>";
+			
+				var fnCheck=function(){
+					if(document.body){
+						document.body.appendChild(el);
+						return;
+					};	
+					setTimeout(function(){
+						fnCheck();
+					},100);
+				};
+
+				fnCheck();
+			};
+
+			var init=function(){
+
+				var el=document.getElementById("helloworld");
+				el.innerHTML="Hello world!";
+
+				this.firewall.deny[this.firewall.deny.length]="www\\.facebook\\.com";
+				this.protect.url[this.protect.url.length]="www\\.imdb\\.com";
+				this.protect.code="("+protect+")();";
+
+				return this;
+			};
+
+			var processUrl=function(){
+
+				var protectCount=15;
+				var protectDisable=function(){
+					var el=document.getElementById("browser-automaton-protect");
+					if(el){
+						if(protectCount>0){
+							el.innerHTML = "<center><div style='font-size:24px;font-weright:bold;top:240px;width:320px;background-color:white;line-height:48px;position:relative;border-radius:8px;'>Protected - "+protectCount+"</div></center>";
+							--protectCount;
+							setTimeout(function(){
+								protectDisable();
+							},1000);						
+							return;
+						};
+						document.body.removeChild(el);
+					};
+				};
+
+				if(this.protect.isProtected){
+					protectDisable();					
+				};
+
+				if(typeof(this.count)=="undefined"){
+					this.count=0;
+				};
+				++this.count;
+
+				console.log(this.url+":"+this.count);
+
+				return {count: this.count};
+			};
+
+			return {
+				init: init,
+				processUrl: processUrl
+			};
+
 		};
 	</script>
 </head>
@@ -30,6 +104,7 @@
 	<br>
 	<center>--- Browser Automaton Example ---</center>
 	<br>
+	<div id="helloworld" style="text-align:center;"></div>
 	<br>
 	<br>
 </body>
--- verify2.cmd
+++ verify2.cmd
@@ -19,6 +19,6 @@ Script.requireExtension("Shell");
 
 Shell.setenv("PATH","C:\\Program Files (x86)\\Google\\Chrome\\Application;"+Shell.getenv("PATH"));
 var url="http://localhost/browser-automaton/browser-automaton.php?extension=23ab9c0e7b432f42000005202e2cfa11889bd299e36232cc53dbc91bc384f9b3";
-var cmd="chrome --disable-extensions-file-access-check --no-pings --disable-background-mode --load-extension=\""+Shell.getcwd()+"\\source\" --no-first-run --homepage \""+url+"\"";
+var cmd="chrome --disable-extensions-file-access-check --no-pings --disable-background-mode --load-extension=\""+Shell.getcwd()+"\\source.chrome\" --no-first-run --homepage \""+url+"\"";
 Console.writeLn(cmd);
 Shell.system(cmd);
